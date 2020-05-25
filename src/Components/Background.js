5ffc43cb5aeeff4c7a46eacc285d2181718f8a40
Daniel Åslund <daniel.aaslund@gmail.com>
2020-05-25 14:26:32 +0200

    changed background image from inline to import reference

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Background.css';
 import Container from 'react-bootstrap/container';
+import background from '../Images/background.png';
 
 const Background = () => {
     return(
         <Container>
-            <img id="backgroundImg" src={require('../Images/background.png')} alt="background"/>
+            <img id="backgroundImg" src={background} alt="background"/>
         </Container>
     );
 }
