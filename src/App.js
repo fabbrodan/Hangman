59a560f37feefeb02bede923044396c516078292
Daniel Åslund <daniel.aaslund@gmail.com>
2020-05-25 15:05:05 +0200

    Changed structure from component background to main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Background from './Components/Background.js';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const rowPaddingTop = {
+  paddingTop: 150
+}
 
 let App = () => {
   return (
-    <Container>
-      <Background/>
+    <Container id="main">
+      <Row style={rowPaddingTop}>
+        <Col lg={{offset: 5, span: 3}} sm={{offset: 5, span: 2}} md={{offset: 5, span: 3}}>
+          <h1>Hänga Gubbe</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{offset: 3, span: 7}} sm={{offset: 5, span: 2}} md={{offset: 5, span: 3}}>
+          <p>
+            Datorn kommer att generera ett slumpmässigt ord som du sedan skall gissa dig fram till.
+            Du har 5 gissningar på dig.
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 }
